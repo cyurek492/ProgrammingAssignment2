4bfef20b319ac270579cbecbabae7c7e5b7183d5
cyurek492 <cyurek001@csbsju.edu>
2024-06-16 21:31:11 -0500

    Add files via upload

---
+++ Project2.R
@@ -0,0 +1,28 @@
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+}
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m 
+  list(set=set,
+       get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse)
+}
+
+cacheSolve <- function(x=matrix(), ...){
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("Getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  return(m)
+}
+
+
